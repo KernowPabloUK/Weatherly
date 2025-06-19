7a2db1d9a8e1b0317f62b684fe16d5663e2b2830
KernowPabloUK <PaulMorganUK1@outlook.com>
2025-06-19 17:17:38 +0100

    CHANGE: Created metrics calculations and tidied js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //#region Global Variables
     const apiKey = "0acafacede1fa597f8b4258fff3abb0d";
-
     const submitButton = document.querySelector("#submit");
 
-    submitButton.addEventListener("click", getWeather);
+    const windDirections = {
+10: "N",
+20: "N/NE",
+30: "N/NE",
+40: "NE",
+50: "NE",
+60: "E/NE",
+70: "E/NE",
+80: "E",
+90: "E",
+100: "E",
+110: "E/SE",
+120: "E/SE",
+130: "SE",
+140: "SE",
+150: "S/SE",
+160: "S/SE",
+170: "S",
+180: "S",
+190: "S",
+200: "S/SW",
+210: "S/SW",
+220: "SW",
+230: "SW",
+240: "W/SW", 
+250: "W/SW",
+260: "W",
+270: "W",
+280: "W",
+290: "W/NW",
+300: "W/NW",
+310: "NW",
+320: "NW",
+330: "N/NW",
+340: "N/NW",
+350: "N",
+360: "N",
+    }
+    //#endregion
 
+    //#region Event Handlers
+    submitButton.addEventListener("click", getWeather);
+    //#endregion
+
+    //#region Functions
     async function getWeather() {
         const location = document.querySelector("#locationInput").value;
         const urlGeo = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`;
@@ -18,31 +61,48 @@ document.addEventListener("DOMContentLoaded", function () {
             const response = await fetch(url);
             const data = await response.json();
             console.log(data);
-            console.log(new Date(data.list[0].dt_txt).getDate());
 
             if (data.cod == 200) {
                 for (let i = 0; i < 5; i++) {
-                    document.querySelector(
-                        `#card${i + 1} .dayOfTheWeek`
-                    ).innerHTML = `${data.list[i * 8].dt_txt}`;
+                    document.querySelector(`#card${i + 1} .dayOfTheWeek`).innerHTML = `${data.list[i * 8].dt_txt}`;
+                    document.querySelector(`#card${i + 1} .sunrise`).innerHTML = `Sunrise<br /><strong>${convertUnixTimeToDateTime(data.city.sunrise)}<strong>`;
+                    document.querySelector(`#card${i + 1} .sunset`).innerHTML = `Sunset<br /><strong>${convertUnixTimeToDateTime(data.city.sunset)}<strong>`;
+                    document.querySelector(`#card${i + 1} .wind-direction`).innerHTML = `Wind Direction<br /><strong>${calculateClosestWindDirection(data, i, windDirections)}<strong>`;
+                    document.querySelector(`#card${i + 1} .wind-speed`).innerHTML = `Wind Speed<br /><strong>${((data.list[i * 8].wind.speed) * 2.23694).toFixed(1)} mph<strong>`;
                 }
             } else {
                 document.querySelector(
-                    "#card1 .dayOfTheWeek"
+                    `.dayOfTheWeek`
                 ).innerHTML = `<p>Location not found.</p>`;
             }
         } catch (error) {
             document.querySelector(
-                "#card1 .dayOfTheWeek"
+                `.dayOfTheWeek`
             ).innerHTML = `<p>Error fetching data.</p>`;
         }
     }
+
+    function convertUnixTimeToDateTime(unixTime) {
+        let date = new Date(unixTime * 1000);
+        let hours = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`;
+        let minutes = date.getMinutes().toString().padStart(2, '0');
+        return `${hours}:${minutes}`;
+    }
+
+    function calculateClosestWindDirection(data, iteration, windDirections) {
+    let windDirectionDegree = data.list[iteration * 8].wind.deg;
+    let closestWindDirection = null;
+    let minDiff = 360;
+    for (let windDegree in windDirections) {
+        let diff = Math.abs(windDirectionDegree - Number(windDegree));
+        if (diff < minDiff) {
+            minDiff = diff;
+            closestWindDirection = windDirections[windDegree];
+        }
+    }
+    return closestWindDirection;
+}
+    //#endregion
+
     // All code above this line
 });
-
-// Location
-// <h2>${data.name}, ${data.sys.country}</h2>
-// <p>Temperature: ${data.main.temp}°C</p>
-//        <p>Weather: ${data.weather[0].description}</p>
-//      <p>Humidity: ${data.main.humidity}%</p>
-//    <p>Wind Speed: ${data.wind.speed} m/s</p>
