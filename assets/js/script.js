ee6eeb8f030d41dfb3003b4c479d4ae8eaaaed46
dav-noble <davidnoble26@gmail.com>
2025-06-20 10:13:43 +0100

    Update: remove dropdown and add media query for screen sizes larger than 992px

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,26 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const submitButton = document.querySelector("#submit");
 
     const weatherIcons = {
-      "01d": "https://openweathermap.org/img/wn/01d@2x.png",
-      "02d": "https://openweathermap.org/img/wn/02d@2x.png",
-      "03d": "https://openweathermap.org/img/wn/03d@2x.png",
-      "04d": "https://openweathermap.org/img/wn/04d@2x.png",
-      "09d": "https://openweathermap.org/img/wn/09d@2x.png",
-      "10d": "https://openweathermap.org/img/wn/10d@2x.png",
-      "11d": "https://openweathermap.org/img/wn/11d@2x.png",
-      "13d": "https://openweathermap.org/img/wn/13d@2x.png",
-      "50d": "https://openweathermap.org/img/wn/50d@2x.png",
-      "01n": "https://openweathermap.org/img/wn/01n@2x.png",
-      "02n": "https://openweathermap.org/img/wn/02n@2x.png",
-      "03n": "https://openweathermap.org/img/wn/03n@2x.png",
-      "04n": "https://openweathermap.org/img/wn/04n@2x.png",
-      "09n": "https://openweathermap.org/img/wn/09n@2x.png",
-      "10n": "https://openweathermap.org/img/wn/10n@2x.png",
-      "11n": "https://openweathermap.org/img/wn/11n@2x.png",
-      "13n": "https://openweathermap.org/img/wn/13n@2x.png",
-      "50n": "https://openweathermap.org/img/wn/50n@2x.png",
-  };
-  
+        "01d": "https://openweathermap.org/img/wn/01d@2x.png",
+        "02d": "https://openweathermap.org/img/wn/02d@2x.png",
+        "03d": "https://openweathermap.org/img/wn/03d@2x.png",
+        "04d": "https://openweathermap.org/img/wn/04d@2x.png",
+        "09d": "https://openweathermap.org/img/wn/09d@2x.png",
+        "10d": "https://openweathermap.org/img/wn/10d@2x.png",
+        "11d": "https://openweathermap.org/img/wn/11d@2x.png",
+        "13d": "https://openweathermap.org/img/wn/13d@2x.png",
+        "50d": "https://openweathermap.org/img/wn/50d@2x.png",
+        "01n": "https://openweathermap.org/img/wn/01n@2x.png",
+        "02n": "https://openweathermap.org/img/wn/02n@2x.png",
+        "03n": "https://openweathermap.org/img/wn/03n@2x.png",
+        "04n": "https://openweathermap.org/img/wn/04n@2x.png",
+        "09n": "https://openweathermap.org/img/wn/09n@2x.png",
+        "10n": "https://openweathermap.org/img/wn/10n@2x.png",
+        "11n": "https://openweathermap.org/img/wn/11n@2x.png",
+        "13n": "https://openweathermap.org/img/wn/13n@2x.png",
+        "50n": "https://openweathermap.org/img/wn/50n@2x.png",
+    };
+
     const weekdays = [
         "Sunday",
         "Monday",
@@ -121,7 +121,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     document.querySelector(
                         `#card${i + 1} .weather-icon`
                     ).innerHTML = `<img src="${iconUrl}" alt="Weather icon">`;
-                    
+
                     // Date Section
                     document.querySelector(
                         `#card${i + 1} .dayOfTheWeek`
@@ -148,11 +148,29 @@ document.addEventListener("DOMContentLoaded", function () {
                     ).innerHTML = `<strong>${Math.round(
                         data.list[i * 8].main.temp
                     )}°C</strong>`;
-                    
-                    document.querySelector(`#card${i + 1} .sunrise`).innerHTML = `Sunrise<br /><strong>${convertUnixTimeToDateTime(data.city.sunrise)}</strong>`;
-                    document.querySelector(`#card${i + 1} .sunset`).innerHTML = `Sunset<br /><strong>${convertUnixTimeToDateTime(data.city.sunset)}</strong>`;
-                    document.querySelector(`#card${i + 1} .wind-direction`).innerHTML = `Wind Direction<br /><strong>${calculateClosestWindDirection(data,i,windDirections)}</strong>`;
-                    document.querySelector(`#card${i + 1} .wind-speed`).innerHTML = `Wind Speed<br /><strong>${(data.list[i * 8].wind.speed * 2.23694).toFixed(1)} mph</strong>`;
+
+                    document.querySelector(
+                        `#card${i + 1} .sunrise`
+                    ).innerHTML = `Sunrise<br /><strong>${convertUnixTimeToDateTime(
+                        data.city.sunrise
+                    )}</strong>`;
+                    document.querySelector(
+                        `#card${i + 1} .sunset`
+                    ).innerHTML = `Sunset<br /><strong>${convertUnixTimeToDateTime(
+                        data.city.sunset
+                    )}</strong>`;
+                    document.querySelector(
+                        `#card${i + 1} .wind-direction`
+                    ).innerHTML = `Wind Direction<br /><strong>${calculateClosestWindDirection(
+                        data,
+                        i,
+                        windDirections
+                    )}</strong>`;
+                    document.querySelector(
+                        `#card${i + 1} .wind-speed`
+                    ).innerHTML = `Wind Speed<br /><strong>${(
+                        data.list[i * 8].wind.speed * 2.23694
+                    ).toFixed(1)} mph</strong>`;
                 }
             } else {
                 document.querySelector(
